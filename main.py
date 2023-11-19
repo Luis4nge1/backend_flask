0b8808aaa44d0e34fd59609fc7ae78c82b6ea7aa
Luis4nge1 <luis24.lmx@gmail.com>
2023-11-19 00:37:07 -0500

    Agregando ultimos cambios

--- main.py
+++ main.py
@@ -16,6 +16,8 @@ from routes.conteo_estado_solicitud_routes import conteo_estado_solicitud_routes
 from routes.solicitudes_estado_routes import solicitudes_estado_routes
 from routes.informacion_solicitante_routes import informacion_solicitante_routes
 from routes.informacion_solicitud_routes import informacion_solicitud_routes
+from routes.informacion_predio import inform_predio
+from routes.solicitud_estado_solicitud import solicitud_es_soli
 from config import DATABASE_CONNECTION_URI
 from flask_cors import CORS
 from utils.db import db
@@ -50,10 +52,11 @@ app.register_blueprint(area_comun_routes)
 app.register_blueprint(predio_area_comun_routes)
 app.register_blueprint(estado_solicitud_routes)
 app.register_blueprint(conteo_estado_solicitud_routes)
-
 app.register_blueprint(solicitudes_estado_routes)
 app.register_blueprint(informacion_solicitante_routes)
 app.register_blueprint(informacion_solicitud_routes)
+app.register_blueprint(inform_predio)
+app.register_blueprint(solicitud_es_soli)
 
 @app.route('/')
 def index():
